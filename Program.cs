bdaca61decd936a2a2c3f8f69bae7c311c2936e8
Jean de Lima Lopes <jean.llopes@outlook.com>
2020-10-11 17:13:30 +0000

    Add use interpolated string

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@ namespace Vote
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name : ");
+            Console.WriteLine("Enter your name :");
                 name = Console.ReadLine();
             Console.WriteLine("Enter your age :");
             int age=int.Parse(Console.ReadLine());
             if(age>=18)
             {
-                Console.WriteLine(name+", you are eligible for voting");
+                Console.WriteLine($"{name}, you are eligible for voting");
             }
             else
             {
-                Console.WriteLine("Sorry "+name+"\nyou are not eligible for voting");
+                Console.WriteLine($"Sorry {name}\nyou are not eligible for voting");
             }
             Console.ReadKey();
         }
