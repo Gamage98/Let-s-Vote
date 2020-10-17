178d98ca8c7765445abf5d8dc1a5aa5c130c0773
DinushiPabasara <72932184+DinushiPabasara@users.noreply.github.com>
2020-10-17 09:00:19 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,13 +16,13 @@ namespace Vote
                 name = Console.ReadLine();
             Console.WriteLine("Enter your age :");
             int age=int.Parse(Console.ReadLine());
-            if(age>=18)
+            if(age<=18)
             {
-                Console.WriteLine($"{name}, you are eligible for voting");
+                Console.WriteLine($"{name}, you are not eligible for voting");
             }
             else
             {
-                Console.WriteLine($"Sorry {name}\nyou are not eligible for voting");
+                Console.WriteLine($"Sorry {name}\nyou are eligible for voting");
             }
             Console.ReadKey();
         }
