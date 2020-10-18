a14844bb79d589c8efb796ac04039415fded069f
Gamage <gamagewijewansa@gmail.com>
2020-10-18 13:05:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,14 +16,21 @@ namespace Vote
                 name = Console.ReadLine();
             Console.WriteLine("Enter your age :");
             int age=int.Parse(Console.ReadLine());
-            if(age<=18)
+            if(age<18)
             {
                 Console.WriteLine($"{name}, you are not eligible for voting");
             }
+            
             else
             {
                 Console.WriteLine($"Sorry {name}\nyou are eligible for voting");
             }
+            
+             if(age==18)
+            {
+                Console.WriteLine($"{name}, you are not eligible for voting Welcome for voting this is your first time you can get the help of guidlines");
+            }
+          
             Console.ReadKey();
         }
     }
